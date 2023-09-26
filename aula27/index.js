47e350305e7c331772853c6c8bc841fe9f051b95
Alana Matos <alanamatosdeoliveira@hotmail.com>
2023-09-26 19:04:17 -0300

    aula27_aprendendo_objetomath

---
+++ aula27/index.js
@@ -0,0 +1,27 @@
+// Objeto Math - exemplos para aprender
+
+let num1 = 9.545412;
+
+//console.log(num1 ** (0.5));  // pegar a raiz quadrada do número
+//console.log(num1 ** (1/2));  // pegar a raiz quadrada do número
+
+
+//let num2 = Math.floor(num1); //arredonda pra baixo
+//let num2 = Math.ceil(num1); // arredonda pra cima
+// let num2 = Math.round(num1); //arredonda para o inteiro mais próximo
+//console.log(num2);
+
+
+
+
+// console.log(Math.max(1, 2, 3, 1500, -10, -50, 9, 8, 7)); //saber qual o maior número da sequência
+// console.log(Math.min(1, 2, 3, 1500, -10, -50, 9, 8, 7)); //saber qual o menor número da sequência
+
+
+
+//console.log(Math.random()); //cada vez que executa, gera um número aleatório
+//const aleatorio = Math.round (Math.random() * (10 - 5) + 5); //um número aleatório entre 10 e 5, arredondado
+//console.log(aleatorio);
+
+
+//console.log(100 / 0);     // retorna um tipo de dado afirmando que é um número infinito e é considerado TRUE
